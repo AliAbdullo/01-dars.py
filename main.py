ca37e45c97a6414da0bf7003469a59ce4b5f385f
AliAbdulloh <74522999+AliAbdullo@users.noreply.github.com>
2022-03-27 06:44:55 +0000

    user info

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-https://replit.com/@anvarbek/javoblar-05-dars?lite=true#main.py
+"""
+8/11/2020
+
+Dasturlash asoslari
+
+#05-dars: STRING (MATN)
+
+Muallif: Anvar Narzullaev
+
+Web sahifa: https://python.sariq.dev
+"""
+
+kocha = "Bog'bon"
+mahalla = "Sog'bon"
+tuman = "Bodomzor"
+viloyat = "Samarqand"
+
+# Yuqoridagi o'zgaruvchilarni jamlab, quyidagi ko'rinishda konsolga chiqaring:
+# Diqqat uzun kodlarni \ belgisi yordamida keyingi qatorga
+# ko'chirish mumkin
+print(kocha + " ko'chasi, " + mahalla + " mahallasi, " + \
+      tuman + " tumani, " + viloyat + " viloyati")
+
+#Yuqoridagi o'zgaruvchilarning qiymatini foydalanuvchidan so'rang.
+print("Iltimos, quyidagi ma'lumotlarni kiriting:")
+ism = input("Ismingiz:")
+kocha = input("Ko'changiz: ")
+mahalla = input("Mahallangiz: ")
+tuman = input("Tumaningiz: ")
+viloyat = input("Viloyatingiz: ")
+
+print(ism + " siz, "+ kocha + " ko'chasi, " + mahalla + " mahallasi, " + \
+      tuman + " tumani, " + viloyat + " viloyatida yashar ekansiz !")   
+tek = input("xatolik yo'qmi tekshirib ko'ring!!!/n>>>")
+
+# Yuqoridagi matnni konsolga chiqarishda har bir verguldan keyin yangi qatorga yozing
+print(kocha + " ko'chasi,\n" + mahalla + " mahallasi,\n" + \
+      tuman + " tumani,\n" + viloyat + " viloyati")
+
+# Yuqoridagi o'zgaruvchilarni f-string yordamida, yangi manzil deb nomlangan o'zgaruvchiga yuklang
+manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+print(manzil)
+
+#manzil ga biz yuqorida o'rgangan metodlarni qo'llab ko'ring.
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.title())
+print(manzil.capitalize())
